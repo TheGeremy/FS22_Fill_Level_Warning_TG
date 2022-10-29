eb7c2e3939dad27b10c7fab00cd911ff42f282a1
TheGeremy <the.geremy@gmail.com>
2022-10-29 10:28:15 +0200

    Create zip_and_move.bat

---
+++ zip_and_move.bat
@@ -0,0 +1,3 @@
+rar a -r FS22_Fill_Level_Warning_TG.zip fillLevelWarning.lua modDesc.xml register.lua icon.dds sounds &
+move /y FS22_Fill_Level_Warning_TG.zip "C:\Users\Jozef\Documents\My Games\FarmingSimulator2022\mods\" &
+dir "C:\Users\Jozef\Documents\My Games\FarmingSimulator2022\mods\FS22_Fill_Level_*.zip"
